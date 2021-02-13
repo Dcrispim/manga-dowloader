523b0aa9a29eecd0f7690f810da4493533f0c66c
Diego Costa <diego.costa@intelie.com.br>
2021-02-13 13:43:05 -0300

    extract Classes

--- utils.py
+++ utils.py
@@ -1,49 +1,16 @@
 import os
+from pathdir import PathDir
 
 from numpy import number
 
 
-class PathDir:
-    def __init__(self, folder):
-        self.folder = folder
-    
-    @property
-    def parent(self):
-        print('inner---', self)
-        return PathDir(os.path.dirname(self.folder))
-    
-    @property
-    def basename(self):
-        return os.path.basename(self.folder)
-
-    def __repr__(self):
-        return self.folder
-
-    def __str__(self):
-        return self.__repr__()
-
-    def abs(self):
-        return os.path.dirname(self.folder)
-
-
-class PathFile(PathDir):
-    def __init__(self, folder):
-        super().__init__(folder)
-    
-    @property
-    def ext(self):
-        return str(self.basename).split('.')[-1]
-    @property
-    def name(self):
-        return str(self.basename).split('.')[0]
-
 def createFolderIfNotExists(folder_path, *folders):
     try:
-        os.mkdir(os.path.join(folder_path, *folders))
+        os.mkdir(os.path.join(str(folder_path), *[str(f) for f in folders]))
     except FileExistsError:
         pass
     except Exception as err:
-        print(err)
+        print(err.__traceback__)
 
 
 def getExtension(file: str or number):
@@ -53,4 +20,9 @@ def getExtension(file: str or number):
 def getParentDir(folder: str):
     return PathDir(folder)
 
-
+def cap_words(name: str):
+    items = []
+    for item in name.split():
+        item = item.capitalize()
+        items.append(item)
+    return ' '.join(items)
