ac9a90918719404fa3fa4f3e38264f4e31cdb945
Diego Costa <diego.costa@intelie.com.br>
2021-02-13 13:43:27 -0300

    create const file

---
+++ consts.py
@@ -0,0 +1,17 @@
+import json
+import getpass
+from utils import PathDir
+import os
+
+ROOT_PATH = PathDir(os.path.join(os.path.dirname(os.path.abspath(__file__))))
+
+CONFIGS = json.load(open(os.path.join(ROOT_PATH.abs, 'config.json'), 'r'))
+
+BASE_DIR = PathDir(
+    CONFIGS["base-dir"].replace('$HOME', f'/home/{getpass.getuser()}'))
+BASE_URL = PathDir(CONFIGS["base-url"])
+TEMP_DIR = PathDir(CONFIGS["temp-dir"])
+
+KINDLE_W_CONST = 1072
+KINDLE_H_CONST = 1448
+
