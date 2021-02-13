778740b0391476ae89bfc350166ff040bfb9c925
Diego Costa <diego.costa@intelie.com.br>
2021-02-13 13:43:53 -0300

    refact

--- manga.py
+++ manga.py
@@ -1,5 +1,6 @@
+from consts import BASE_DIR, BASE_URL, ROOT_PATH
 import os
-from utils import createFolderIfNotExists
+from utils import PathDir, createFolderIfNotExists, getExtension
 from pdfconverter import convertFolder, fit_images_by_folder, fix_images_by_folder, isDuplicate
 import cv2
 import getpass
@@ -9,10 +10,6 @@ import requests
 
 args = os.sys.argv[1:]
 
-
-BASE_URL = 'https://cdn.mangayabu.top/mangas'
-BASE_DIR = f'/home/{getpass.getuser()}/Documents/mangas'
-slime = 'tensei-shitara-slime-datta-ken'
 mangaName = args[0]
 mangaEndPage = args[1]
 mangaStartPage = 1
@@ -25,7 +22,6 @@ cleanCap = False
 global status
 
 status = ''
-
 
 
 for arg in args:
@@ -59,10 +55,11 @@ for arg in args:
 createFolderIfNotExists(BASE_DIR)
 createFolderIfNotExists(BASE_DIR, '.temp')
 
+
 def get_manga_images_link(name: str, cap: int, endPage: int, startPage=1):
     decimal = ''
     if len(str(cap).split('.')) == 2:
-        decimal = f'.{str(cap).split(".")[-1]}'
+        decimal = f'.{getExtension(cap)}'
 
     if cleanCap:
         request = [
@@ -77,9 +74,13 @@ def get_manga_images_link(name: str, cap: int, endPage: int, startPage=1):
 def getNameCap(name, cap, dirname=None):
     decimal = ''
     if len(str(cap).split('.')) == 2:
-        decimal = f'.{str(cap).split(".")[-1]}'
+        decimal = f'.{getExtension(cap)}'
 
-    return f'{dirname or BASE_DIR}/{name}/{name}-cap{str(cap).split(".")[0]:0>4}{decimal}'
+    clearCap = str(cap).split(".")[0]
+    nameCap = f'{name}-cap{clearCap:0>4}{decimal if float(decimal or str(0))>0 else ""}'
+    dirname = PathDir(dirname or BASE_DIR)
+
+    return dirname.join(name, nameCap)
 
 
 def log_bar(value, min=0, max=100, lenBar=100, msg='', emptyChar='-', fullChar='='):
@@ -87,14 +88,15 @@ def log_bar(value, min=0, max=100, lenBar=100, msg='', emptyChar='-', fullChar='
         lenFull = int((value/(max-min))*lenBar)
     except ZeroDivisionError:
         lenFull = int((value/(max))*lenBar)
-    #os.system('clear')
+    # os.system('clear')
     fullBar = ''.join([fullChar for i in range(0, lenFull+1)])
     emptyBar = ''.join([emptyChar for i in range(0, lenBar-lenFull+1)])
     return f"{msg}\n\n{fullBar}{emptyBar}"
 
+
 def download_image(pic_url, name):
-    with open(name, 'wb') as handle:
-        response = requests.get(pic_url, stream=True)
+    with open(str(name), 'wb') as handle:
+        response = requests.get(str(pic_url), stream=True)
 
         if not response.ok:
             print(response)
@@ -106,37 +108,39 @@ def download_image(pic_url, name):
             handle.write(block)
 
 
-
 def download_images(name, cap, dirName=None, title='', percent=None):
     pages_link = get_manga_images_link(name, cap, 100)
 
     if dirName == None:
         createFolderIfNotExists(getNameCap(name, cap))
 
-    cmds = []
     for page in pages_link:
+        page_dir = PathDir(page)
         os.system('clear')
         if percent:
             os.system(
-                f"{os.path.join(os.path.dirname(os.path.abspath(__file__)),'progressbarr.sh')} {int(percent*100)} '{title}'")
+                f"{ROOT_PATH.join('progressbarr.sh')} {int(percent*100)} '{title}'")
         else:
-            os.system(f"./progressbarr.sh 0 '{title}'")
-        
-        nameImage = os.path.join((dirName or f"{getNameCap(name,cap)}"),f"{page.split('/')[-1]}")
+            os.system(f"{ROOT_PATH.join('progressbarr.sh')} 0 '{title}'")
+
+        nameImage = PathDir((dirName or f"{getNameCap(name,cap)}")).join(
+            page_dir.basename)
         print(page)
         try:
             download_image(page, nameImage)
-            isdub= isDuplicate(nameImage,(dirName or f"{getNameCap(name,cap)}"))
+            isdub = isDuplicate(
+                nameImage, (dirName or f"{getNameCap(name,cap)}"))
             if isdub:
                 print(isdub)
                 break
         except Exception as err:
-            print(err)
+
+            print(err, 'line 138')
             break
         finally:
-            if int(os.path.getsize(nameImage))<10:
+            if nameImage.size < 10:
                 print(nameImage)
-                os.remove(nameImage)
+                os.remove(nameImage.abs)
 
 
 def download_manga(name, end, start=1, dirname=None, especials=[], exclude=[]):
@@ -147,41 +151,43 @@ def download_manga(name, end, start=1, dirname=None, especials=[], exclude=[]):
     caps = [str(cap) for cap in range(int(start or '1'), int(end)+1)]
     caps.extend([str(c) for c in especials])
     caps.sort(key=sortNumberString)
-    manganame = os.path.join(dirname or BASE_DIR, name)
+
+    dir_name = PathDir(dirname or BASE_DIR)
+    manganame = dir_name.join(name)
 
     createFolderIfNotExists(manganame)
-    createFolderIfNotExists(os.path.join(manganame, 'jpgs'))
+    createFolderIfNotExists(manganame.join('jpgs'))
 
     i = 1
     for cap in caps:
 
         if cap not in exclude:
 
-            dirnamecapname = getNameCap(name, cap, dirname)
+            dirnamecapname = getNameCap(name, cap, dir_name)
             namecap = ' '.join([c.capitalize()
-                                for c in dirnamecapname.split('/')[-1].split('-')])
-            dirJPGcapname = os.path.join(
+                                for c in dirnamecapname.basename.split('-')])
+            dirJPGcapname = PathDir(
                 manganame, 'jpgs', namecap.replace(' ', '_').lower())
-            
+
             createFolderIfNotExists(dirJPGcapname)
 
-            download_images(name, cap, dirJPGcapname, namecap,
-                            i/(len(caps)-len(exclude)))
+            download_images(name, cap, dirJPGcapname.abs, namecap,i/(len(caps)-len(exclude)))
             fix_images_by_folder(dirJPGcapname)
             fit_images_by_folder(dirJPGcapname)
             convertFolder(dirJPGcapname, manganame, namecap)
             print(dirJPGcapname, '\n')
         i += 1
-    #os.system('clear')
+    # os.system('clear')
     print(f'finish {name}')
 
-with open('./log','a+') as log:
+
+with open('./log', 'a+') as log:
     log.write(f'{datetime.now()}: START {mangaName} cap-{mangaStartPage} to cap-{mangaStartPage} and {especials} EXCLUDING {exclude}\n')
 
 
 download_manga(mangaName, mangaEndPage, mangaStartPage,
-              mangaDirName,  especials, exclude)
+               mangaDirName,  especials, exclude)
 
 
-with open('./log','a+') as log:
+with open('./log', 'a+') as log:
     log.write(f'{datetime.now()} END {mangaName}\n')
