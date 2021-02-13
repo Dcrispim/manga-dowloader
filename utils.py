df8830a856d0d3e5a6d333f1e4f2fb6c03097690
Diego Costa <diego.costa@intelie.com.br>
2021-02-13 09:51:45 -0300

    create path classes

--- utils.py
+++ utils.py
@@ -1,5 +1,41 @@
 import os
 
+from numpy import number
+
+
+class PathDir:
+    def __init__(self, folder):
+        self.folder = folder
+    
+    @property
+    def parent(self):
+        print('inner---', self)
+        return PathDir(os.path.dirname(self.folder))
+    
+    @property
+    def basename(self):
+        return os.path.basename(self.folder)
+
+    def __repr__(self):
+        return self.folder
+
+    def __str__(self):
+        return self.__repr__()
+
+    def abs(self):
+        return os.path.dirname(self.folder)
+
+
+class PathFile(PathDir):
+    def __init__(self, folder):
+        super().__init__(folder)
+    
+    @property
+    def ext(self):
+        return str(self.basename).split('.')[-1]
+    @property
+    def name(self):
+        return str(self.basename).split('.')[0]
 
 def createFolderIfNotExists(folder_path, *folders):
     try:
@@ -8,3 +44,13 @@ def createFolderIfNotExists(folder_path, *folders):
         pass
     except Exception as err:
         print(err)
+
+
+def getExtension(file: str or number):
+    return str(file).split('.')[-1]
+
+
+def getParentDir(folder: str):
+    return PathDir(folder)
+
+
